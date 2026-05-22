e5e89ba40595225c17ce17cbe867b7982a6134b3
Hélio Coelho <81621491+helinhocoelho@users.noreply.github.com>
2026-05-22 19:54:14 -0300

    Teste de login 2

--- app.js
+++ app.js
@@ -3,62 +3,31 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabaseUrl = "https://ubhbapccfqvcetfazxjw.supabase.co";
-
 const supabaseKey = "sb_publishable_wFCLK5zs-_TqRFcbuwGBhg_gs_LiZHm";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const loginBtn = document.getElementById("loginBtn");
-
-loginBtn.addEventListener("click", async () => {
-  const email = document.getElementById("email").value;
-
-  const senha = document.getElementById("senha").value;
-
+// LOGIN
+async function login(email, password) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
-    password: senha,
+    password,
   });
 
   if (error) {
-    alert("Erro login");
-
-    console.log(error);
-
+    console.log("Erro login:", error.message);
     return;
   }
 
-  alert("Login realizado");
+  console.log("LOGADO:", data.user);
 
-  carregarClientes();
-});
-
-async function carregarClientes() {
-  const { data, error } = await supabase.from("clientes").select("*");
-
-  console.log(data);
-
-  const container = document.getElementById("clientes");
-
-  container.innerHTML = "";
-
-  data.forEach((cliente) => {
-    container.innerHTML += `
-      <div class="card">
-
-        <h2>${cliente.nome}</h2>
-
-        <p>
-          Projeto:
-          ${cliente.projeto}
-        </p>
-
-        <p>
-          Status:
-          ${cliente.status}
-        </p>
-
-      </div>
-    `;
-  });
+  carregarDados(); // chama seu dashboard
 }
+
+// exemplo de uso
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  login(email, senha);
+});
