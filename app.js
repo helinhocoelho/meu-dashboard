317caeabc78a0458a20d12aa9d3856e4703f7d8b
Hélio Coelho <81621491+helinhocoelho@users.noreply.github.com>
2026-05-22 19:21:30 -0300

    Primeira lógica

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-// import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-// const supabaseUrl = "https://ubhbapccfqvcetfazxjw.supabase.co";
-
-// const supabaseKey = "sb_publishable_wFCLK5zs-_TqRFcbuwGBhg_gs_LiZHm";
-
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// console.log("Supabase conectado");
+//supabaseKey: sb_publishable_wFCLK5zs-_TqRFcbuwGBhg_gs_LiZHm
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
@@ -19,9 +11,34 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 async function carregarClientes() {
   const { data, error } = await supabase.from("clientes").select("*");
 
-  console.log("DADOS:", data);
+  console.log(data);
 
-  console.log("ERRO:", error);
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const container = document.getElementById("clientes");
+
+  data.forEach((cliente) => {
+    container.innerHTML += `
+      <div class="card">
+
+        <h2>${cliente.nome}</h2>
+
+        <p>
+          <strong>Projeto:</strong>
+          ${cliente.projeto}
+        </p>
+
+        <p>
+          <strong>Status:</strong>
+          ${cliente.status}
+        </p>
+
+      </div>
+    `;
+  });
 }
 
 carregarClientes();
